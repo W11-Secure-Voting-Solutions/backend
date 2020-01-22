31e3f7dff19ef09052b88b088cd63f68b7cd243a
piotrszyma <thompson2908@gmail.com>
2020-01-22 23:37:34 +0100

    Removal of dead code of assistant views (#24)

--- server/apolloassistant/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from helios.view_utils import return_json
-
-
-# TODO: Fill with bulletin board.
-@return_json
-def get_bulletin_board(request):
-    return []
