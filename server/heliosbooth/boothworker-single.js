4e63d99445b30aee89c92c3dac5ae1481b2f244a
Piotr Szyma <thompson2908@gmail.com>
2020-01-24 01:48:03 +0100

    Disabled compression of js

--- server/heliosbooth/boothworker-single.js
+++ server/heliosbooth/boothworker-single.js
@@ -6,19 +6,19 @@
 importScripts("js/underscore-min.js");
 
 importScripts("js/jscrypto/jsbn.js",
-	      "js/jscrypto/jsbn2.js",
-	      "js/jscrypto/sjcl.js",
-	      "js/jscrypto/class.js",
-	      "js/jscrypto/bigint.js",
-	      "js/jscrypto/random.js",
-	      "js/jscrypto/elgamal.js",
-	      "js/jscrypto/sha1.js",
-	      "js/jscrypto/sha2.js",
-	      "js/jscrypto/helios.js");
+    "js/jscrypto/jsbn2.js",
+    "js/jscrypto/sjcl.js",
+    "js/jscrypto/class.js",
+    "js/jscrypto/bigint.js",
+    "js/jscrypto/random.js",
+    "js/jscrypto/elgamal.js",
+    "js/jscrypto/sha1.js",
+    "js/jscrypto/sha2.js",
+    "js/jscrypto/helios.js");
 
 var console = {
-    'log' : function(msg) {
-    	self.postMessage({'type':'log','msg':msg});
+    'log': function (msg) {
+        self.postMessage({ 'type': 'log', 'msg': msg });
     }
 };
 
@@ -39,11 +39,11 @@ function do_encrypt(message) {
 
     // send the result back
     self.postMessage({
-	    'type': 'result',
-      'q_num': message.q_num,
-		  'encrypted_answer': encrypted_answer.toJSONObject(true),
-		  'id':message.id
-		});
+        'type': 'result',
+        'q_num': message.q_num,
+        'encrypted_answer': encrypted_answer.toJSONObject(true),
+        'id': message.id
+    });
 }
 
 // receive either
@@ -54,11 +54,11 @@ function do_encrypt(message) {
 // b) an answer that needs encrypting
 // {'type': 'encrypt', 'q_num': 2, 'id': id, 'answer': answer_json}
 //
-self.onmessage = function(event) {
+self.onmessage = function (event) {
     // dispatch to method
     if (event.data.type === "setup") {
         do_setup(event.data);
-	} else if (event.data.type === "encrypt") {
+    } else if (event.data.type === "encrypt") {
         do_encrypt(event.data);
     }
 };
--- server/heliosbooth/js/jscrypto/bigint.dummy.js
+++ server/heliosbooth/js/jscrypto/bigint.dummy.js
@@ -9,54 +9,54 @@
 // generally being a nice JavaScript object.
 
 BigIntDummy = Class.extend({
-	init: function(value, radix) {
-	    if (radix != 10)
-		throw "in dummy, only radix=10, here radix=" + radix;
-	    
-	    this.value = value;
+	init: function (value, radix) {
+		if (radix != 10)
+			throw "in dummy, only radix=10, here radix=" + radix;
+
+		this.value = value;
 	},
-  
-	toString: function() {
-	    return this.value;
+
+	toString: function () {
+		return this.value;
 	},
-  
-	toJSONObject: function() {
-	    // toString is apparently not overridden in IE, so we reproduce it here.
-	    return this.value;
+
+	toJSONObject: function () {
+		// toString is apparently not overridden in IE, so we reproduce it here.
+		return this.value;
 	},
-  
-	add: function(other) {
-	    throw "dummy, no add!";
+
+	add: function (other) {
+		throw "dummy, no add!";
 	},
-  
-	bitLength: function() {
-	    throw "dummy, nobitlength!";
+
+	bitLength: function () {
+		throw "dummy, nobitlength!";
 	},
-  
-	mod: function(modulus) {
-	    throw "dummy, no mod!";
+
+	mod: function (modulus) {
+		throw "dummy, no mod!";
 	},
-  
-	equals: function(other) {
-	    throw "dummy, no equals!";
+
+	equals: function (other) {
+		throw "dummy, no equals!";
 	},
-  
-	modPow: function(exp, modulus) {
-	    throw "dummy, no modpow!";
+
+	modPow: function (exp, modulus) {
+		throw "dummy, no modpow!";
 	},
-	
-	negate: function() {
-	    throw "dummy, no negate!";
+
+	negate: function () {
+		throw "dummy, no negate!";
 	},
-  
-	multiply: function(other) {
-	    throw "dummy, no multiply!";
+
+	multiply: function (other) {
+		throw "dummy, no multiply!";
 	},
-  
-	modInverse: function(modulus) {
-	    throw "dummy, no modInverse";
+
+	modInverse: function (modulus) {
+		throw "dummy, no modInverse";
 	}
-  
+
 });
 
 BigIntDummy.use_applet = false;
@@ -64,22 +64,22 @@ BigIntDummy.use_applet = false;
 BigIntDummy.is_dummy = true;
 BigIntDummy.in_browser = false;
 
-BigIntDummy.fromJSONObject = function(s) {
-  return new BigIntDummy(s, 10);
+BigIntDummy.fromJSONObject = function (s) {
+	return new BigIntDummy(s, 10);
 };
 
-BigIntDummy.fromInt = function(i) {
-  return BigIntDummy.fromJSONObject("" + i);
+BigIntDummy.fromInt = function (i) {
+	return BigIntDummy.fromJSONObject("" + i);
 };
 
-BigIntDummy.ZERO = new BigIntDummy("0",10);
-BigIntDummy.ONE = new BigIntDummy("1",10);
-BigIntDummy.TWO = new BigIntDummy("2",10);
-BigIntDummy.FORTY_TWO = new BigIntDummy("42",10);
+BigIntDummy.ZERO = new BigIntDummy("0", 10);
+BigIntDummy.ONE = new BigIntDummy("1", 10);
+BigIntDummy.TWO = new BigIntDummy("2", 10);
+BigIntDummy.FORTY_TWO = new BigIntDummy("42", 10);
 
 BigIntDummy.ready_p = true;
 
-BigIntDummy.setup = function(callback, fail_callback) {
-    //console.log("using dummy bigint");
-  callback();
+BigIntDummy.setup = function (callback, fail_callback) {
+	//console.log("using dummy bigint");
+	callback();
 }
